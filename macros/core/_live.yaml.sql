5df67da3e333d3e49c92ef1407b2f3fb9bc58145
shah <info@shahnewazkhan.ca>
2025-04-08 10:43:03 -0700

    add lt_near_tx_udf_api

--- macros/core/_live.yaml.sql
+++ macros/core/_live.yaml.sql
@@ -14,4 +14,20 @@
   options: |
     NOT NULL
   sql: udf_api
+
+- name: {{ schema }}.lt_near_tx_udf_api
+  signature:
+    - [method, STRING]
+    - [url, STRING]
+    - [headers, OBJECT]
+    - [DATA, VARIANT]
+    - [user_id, STRING]
+    - [SECRET, STRING]
+  return_type: VARIANT
+  func_type: EXTERNAL
+  api_integration: '{{ var("API_INTEGRATION") }}'
+  options: |
+    NOT NULL
+    MAX_BATCH_ROWS = 25
+  sql: udf_api
 {% endmacro %}
